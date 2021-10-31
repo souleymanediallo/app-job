38aa1b856328f878616b641ece82367bdda6a4f9
souleymane <souleymanedialloo@yahoo.fr>
2021-10-31 21:06:54 +0100

    add register

---
+++ accounts/forms.py
@@ -0,0 +1,21 @@
+from django import forms
+from django.contrib.auth.forms import UserCreationForm
+from .models import CustomUser
+
+# https://docs.djangoproject.com/en/3.2/ref/forms/widgets/
+
+
+class CustomUserForm(UserCreationForm):
+    CHOICES = (('is_employer', 'employer'), ('is_employee', 'employee'))
+    user_type = forms.CharField(label="Type de compte", widget=forms.RadioSelect(choices=CHOICES))
+
+    class Meta:
+        model = CustomUser
+        fields = ["email", "password1", "password2"]
+
+    def __init__(self, *args, **kwargs):
+        super(CustomUserForm, self).__init__(*args, **kwargs)
+        for name, field in self.fields.items():
+            field.widget.attrs.update({'class': 'form-control'})
+
+        self.fields['user_type'].widget.attrs.update({'class': 'form-check-input'})
--- accounts/views.py
+++ accounts/views.py
@@ -1,3 +1,24 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.messages.views import SuccessMessageMixin
+from django.views.generic import CreateView
+from .models import CustomUser
+from .forms import CustomUserForm
+
 
 # Create your views here.
+class UserRegisterView(SuccessMessageMixin, CreateView):
+    form_class = CustomUserForm
+    template_name = "accounts/register.html"
+    success_url = "/"
+    success_message = "Votre compte a été crée !"
+
+    def form_valid(self, form):
+        user = form.save(commit=False)
+        user_type = form.cleaned_data['user_type']
+        if user_type == "is_employee":
+            user.is_employee = True
+        elif user_type == "is_employer":
+            user.is_employer = True
+        user.save()
+
+        return redirect(self.success_url)
